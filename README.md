ca6002edd91891a1b367afd68213dca86604ec71
daniele <daniele.didino@gmail.com>
2024-06-18 16:15:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<h1 align="center">D-AI-logue App</h1>
+
+<p align="center">A System for Monitoring and Improving Online Language</p>
+
+Thank you for your interest in our project. Please be aware that this is still a prototipe and may contain bugs or unfinished features.
+
+This project aims to develop tools that can detect dysfunctinal and toxic language in chat conversations and provide suggestions to make the language more respectful and inclusive.
+
+*MORE INFO COMING SOON*
+
+### Synthetic data
+
+Both `gpt-3.5-turbo` and `dolphin-mistral` were used to generate synthetic data (see files `gen_data_gpt.py`  and `gen_data_dolphin.py`).
+
+The output from the `dolphin-mistral` model was unstable and frequently did not conform to a JSON file format.
+
+To resolve this, I implemented the following:
+
+- Introduced a `system message`: "You are an AI assistant that outputs only JSON data. Do not include any text before or after the JSON response."
+- Reiterated the desired format in the `prompt`: "Always respond only with valid JSON format and nothing else. Do not include any text before or after the JSON."
+
+But also with these adjustments, the model now does not generate the desired output format.
+
+So I added in the for loop a way to check if the output is correct and if not it re-run the model.
---
+++ gen_data_dolphin.py
@@ -0,0 +1,144 @@
+import environ
+import sys
+import json
+import csv
+from pathlib import Path
+from datetime import datetime
+import ollama
+
+from synthetic_data.issues_category import issues
+
+# Set constanst
+LLM_MODEL = "dolphin-mistral"
+
+# How many synthetic sentences generate for each issue
+N_PAIRS = 5
+
+
+def call_model(prompt: str, system_message: str, model_name: str) -> tuple[str, str]:
+    """
+    Take a prompt and a model name and use the ollama framework to 
+    generate a response.
+    It returns a tuple with the model name and the response.
+    """
+
+    response = ollama.generate(
+            model=model_name,
+            prompt=prompt,
+            system=system_message)
+        
+    return response["response"]
+
+
+def generate_data(issues: list, n_pairs: int) -> list:
+    
+    # List to store the reposnes
+    responses_json = []
+
+    # Count the iteration throught the "issues" list
+    N_issue = 1
+
+    for issue in issues:
+
+        # Print the current issue number
+        print(f"Generating output {N_issue} of {len(issues)}")
+        N_issue += 1
+
+        system_message = """
+        You are an AI assistant that outputs only JSON data.
+        Do not include any text before or after the JSON response.
+        """
+
+        prompt_1 = f"""
+        Generate examples of dysfunctional and toxic language that might be encountered between couples or 
+        ex-couples who have to continuously interact.
+    
+        Each entry should include:
+    
+        1 - A sentence reflecting dysfunctional communication, showcasing various forms of toxicity such as
+            insults, harassment, threats, manipulation, and derogatory remarks.
+        
+        2 - A transformed version of the same sentence that represents functional, healthy communication.
+    
+        Ensure the sentences are realistic and diverse in terms of content and context.
+        The sentences should refer to this issue category:
+        '{issue}'
+    
+        Provide {n_pairs} pairs of sentences.
+        """
+    
+        # The prompt is divided into 2 sub-prompts because
+        # the example of the output format uses Curly brackets {},
+        # and this cannot be done in a f-string.
+        prompt_2 = """
+        Always respond only with valid JSON format and nothing else.
+        Do not include any text before or after the JSON.
+
+        You must provide the output exactly in the following format:
+        
+        [
+            {
+                "dysfunctional": "write here the dysfunctional text",
+                "functional": "write here the functional text"
+            },
+            {
+                "dysfunctional": "write here the dysfunctional text",
+                "functional": "write here the functional text"
+            },
+        ]
+        """
+    
+        prompt = prompt_1 + prompt_2
+
+        max_iteration = 3
+        num_tries = 0
+        
+        while True:
+            # m, r = call_model(prompt, model_name=LLM_MODEL)
+            r = call_model(prompt, system_message, LLM_MODEL)
+
+            # Check if output is valid JSON
+            try:
+                responses_json += json.loads(r)
+                break
+            except json.JSONDecodeError:
+                num_tries += 1
+                if num_tries >= max_iteration:
+                    print(" "*4 + f"Invalid JSON format after {max_iteration} retries. Aborting...")
+                    break
+                print(" "*4 + "Invalid JSON format. Re-running model...")
+
+    return responses_json
+
+
+def save_files(responses_json: list, path_json: Path, path_csv: Path):
+
+    print(f"There are {len(responses_json)} pairs generated sentences")
+    print(f"Expected number of pairs: {len(issues) * N_PAIRS}")
+
+    print("Saving json file")
+    with path_json.open("w") as f:
+        json.dump(responses_json, f, indent=4)
+    
+    print("Saving csv file")
+    keys = responses_json[0].keys()
+    with path_csv.open("w", newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=keys)
+        writer.writeheader()
+        writer.writerows(responses_json)
+
+
+if __name__ == "__main__":
+
+    print("###### Start generating data ######")
+    response = generate_data(issues, N_PAIRS)
+    print("############## Done ###############")
+    
+    print("######## Start storing data #######")
+    now = datetime.now()
+    date_string = now.strftime("%Y-%m-%d_%H-%M")
+    filename = f"synthetic_data_dolphin_{date_string}"
+    path_json = Path("./synthetic_data", filename + ".json")
+    path_csv = Path("./synthetic_data", filename + ".csv")
+    save_files(response, path_json, path_csv)
+    print("############## Done ###############")
