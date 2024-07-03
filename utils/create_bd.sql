b18bd773814bc0c3f62983d75a56def517d5f11d
daniele <daniele.didino@gmail.com>
2024-07-03 16:57:30 +0200

    implementing functions for dynamic few-shot prompt

--- utils/create_bd.sql
+++ utils/create_bd.sql
@@ -1,6 +1,15 @@
-CREATE TABLE examples (
-    id SERIAL PRIMARY KEY,
-    dysfunctional TEXT NOT NULL,
-    embedding TEXT NOT NULL,
-    functional TEXT NOT NULL
-);
+-- First version: embedding as "TEXT"
+-- CREATE TABLE examples (
+--     id SERIAL PRIMARY KEY,
+--     dysfunctional TEXT NOT NULL,
+--     embedding TEXT NOT NULL,
+--     functional TEXT NOT NULL
+-- );
+
+-- Embedding as "BLOB" (Binary Large Object)
+CREATE TABLE IF NOT EXISTS examples (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    dysfunctional TEXT,
+    embedding BLOB,
+    functional TEXT
+)
